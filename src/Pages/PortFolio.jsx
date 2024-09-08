6ffa31ffa097c10c549876bf3effd6ef9dae8463
PiyushSharma31112 <piyushsharmadec009@gmail.com>
2024-09-08 14:53:11 +0530

    add some projects info

--- src/Pages/PortFolio.jsx
+++ src/Pages/PortFolio.jsx
@@ -23,7 +23,7 @@ function PortFolio() {
     <div className='content-box ' data-aos = "fade-down" da-aos-duration= "1200">
         <PageHeading mainText= {"My"} highLightedText = {"Portfolio"} secondText = {"works"} />
 
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-8 mb-10'>
           {
               PortfolioData.map(data => (
                 <PortfolioCard key={data.id} {...data} />
--- src/data/PortfolioData.js
+++ src/data/PortfolioData.js
@@ -10,6 +10,18 @@ const PortfolioData = [
         projectTitle: "Quiz App",
         thumbnail: "https://www.shutterstock.com/image-vector/template-mobile-application-interface-quiz-260nw-2183926059.jpg",
         previewLink: "https://quizapponlyfrontend.netlify.app/",
+    },
+    {
+        id: 3,
+        projectTitle: "Weather App",
+        thumbnail: "https://www.lifewire.com/thmb/XuBOb9AnIzEtx0dtV0QQylP8LSE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/003-The6BestWeatherAppsforiPhone-37d612cde23b49d8bba445223a1ae059.jpg",
+        previewLink: "https://neon-cucurucho-ce0336.netlify.app"
+    },
+    {
+        id: 4,
+        projectTitle: "Movie search App",
+        thumbnail: "https://devchallenges.io/_next/image?url=https%3A%2F%2Fcsyxkpbavpcrhwqhcpyy.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fchallenges%2F40%2Fchallenge-40-thumbnail&w=1920&q=75",
+        previewLink: ""
     }
 ]
 
