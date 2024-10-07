99bc284db6fad7ec17339e4a40fcfddf7d56b60f
PiyushSharma31112 <piyushsharmadec009@gmail.com>
2024-10-07 23:03:04 +0530

    Features: Fix page heading

--- src/components/common/PageHeading.jsx
+++ src/components/common/PageHeading.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function PageHeading({mainText,highLightedText,secondText}) {
     return (
         <div className='md:py-20 py-10 text-left md:text-center relative'>
-            <h1 className='text-3xl md:text-[56px] font-extrabold uppercase text-[#616262] dark:text-white'>{mainText}
+            <h1 className='main-text text-3xl md:text-[56px] font-extrabold uppercase text-[#616262] dark:text-white'>{mainText}
             &nbsp;
                 <span className='text-primary-yellow'>{highLightedText}</span>
             </h1>
-            <span className='md:text-[45px] lg:text-[110px] tracking-[10px] font-extrabold dark:text-[#ffffff12] uppercase absolute top-1/2 left-1/2 -translate-x-1/2 md:-translate-y-4  lg:-translate-y-1/2 text-[#1e253012]'>{secondText}</span>
+            <span className='md:text-[45px] second-text lg:text-[110px] tracking-[10px] font-extrabold dark:text-[#ffffff12] uppercase absolute top-1/2 left-1/2 -translate-x-1/2 md:-translate-y-4  lg:-translate-y-1/2 text-[#1e253012]'>{secondText}</span>
         </div>
     )
 }
